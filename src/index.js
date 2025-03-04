e4bac3b46d542d33d8247bbd4231e5a329395d75
Shristy1503 <shriimishra15@gmail.com>
2025-03-04 13:29:43 +0530

    [Shristy] Add.UC2 Functionality

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const AddressBook = require('./service/AddressBook');
 const Contact = require('./model/Contact');
 
 const addressBook = new AddressBook();
-
+try{
 // Create contacts
-const contact1 = new Contact("Shristy", "Mishra", "Ratnagiri", "Bhopal", "M.P.", "123456", "9876543210", "shriimishra@gmail.com");
-const contact2 = new Contact("Arjun", "Singh", "ABc", "Bhopal", "M.P", "789101", "8765432109", "arjun@email.com");
+const contact1 = new Contact("Shristy", "Mishra", "Ratnagiri", "Bhopal", "Madhya", "123456", "9876543210", "shriimishra@gmail.com");
+const contact2 = new Contact("Arjun", "Singh", "Anand", "Bhopal", "Madhya Pradesh", "789101", "8765432109", "arjun@email.com");
 
 console.log(addressBook.addContact(contact1)); 
 console.log(addressBook.addContact(contact2));
@@ -13,4 +13,16 @@ console.log(addressBook.addContact(contact2));
 console.log("All Contacts:", addressBook.getAllContacts());
 
 console.log("Search Result:", addressBook.searchContact("Arjun"));
+}catch(error){
+    console.error("Error:", error.message);
+}
+
+try {
+    const invalidContact = new Contact(
+        "abc", "def", "ghi", "In", "mp", "1001", "12345", "shristy-email"
+    );
+    console.log(addressBook.addContact(invalidContact));
+} catch (error) {
+    console.error("Error:", error.message);
+}
 
